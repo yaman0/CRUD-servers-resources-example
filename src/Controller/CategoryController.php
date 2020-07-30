22e303f5108cd00587c9ad3c2bc285c294c73d47
yaman0 <yaman0@protonmail.com>
2020-07-30 13:30:32 +0200

    :sparkles: CRUD categories OK

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,18 +3,38 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/category", name="category")
+     * @var CategoryRepository
+     */
+    private $repository;
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    public function __construct(CategoryRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/category", name="category.index")
      */
     public function index()
     {
-        $categories = $this->getDoctrine()->getRepository(Category::class)
-            ->findAll();
+        $categories = $this->repository->findAll();
         return $this->render('category/index.html.twig', [
             "categories" => $categories,
             'controller_name' => 'CategoryController'
@@ -22,16 +42,62 @@ class CategoryController extends AbstractController
     }
 
     /**
-     * @Route("/category/{id}", name="category_show")
-     * @param int $id
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("category/create", name="category.new")
+     * @param Request $req
+     * @return RedirectResponse|Response
      */
-    public function show(int $id)
+    public function new(Request $req)
     {
-        $category = $this->getDoctrine()->getRepository(Category::class)
-            ->find($id);
-        return $this->render('category/show.html.twig', [
-            "category" => $category
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Create Ok');
+            $this->em->persist($category);
+            $this->em->flush();
+            return $this->redirectToRoute('category.index');
+        }
+
+        return $this->render('category/new.html.twig', [
+            "category" => $category,
+            "form" => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/category/{id}", name="category.edit", methods={"GET","POST"})
+     * @param Category $category
+     * @return Response
+     */
+    function edit(Request $req, Category $category)
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Edit Ok');
+            $this->em->flush();
+            return $this->redirectToRoute('category.index');
+        }
+        return $this->render('category/edit.html.twig', [
+            "category" => $category,
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/category/{id}", name="category.delete", methods={"DELETE"})
+     * @param Category $category
+     */
+    function delete(Request $req, Category $category)
+    {
+        if ($this->isCsrfTokenValid('delete' . $category->getId(), $req->get('_token'))) {
+            $this->addFlash('success', 'Delete Ok');
+            $this->em->remove($category);
+            $this->em->flush();
+        }
+        return $this->redirectToRoute('category.index');
+    }
+
 }
